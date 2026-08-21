6b5be82ad2de3d80ef5f17cf3c270a03892fda1a
erosfrancesco <eros.francesco.francesco@gmail.com>
2026-08-21 12:47:15 +0200

    tested ok

--- test.js
+++ test.js
@@ -1,19 +1,21 @@
-//    ws://192.168.0.101:8765/
-const ws = new WebSocket("ws://192.168.0.101:8765");
+const ws = new WebSocket("ws://192.168.1.101:8765");
 
 ws.onopen = () => {
-  console.log("Connected");
-  ws.send("Hello server");
+    console.log("Connected");
+
+    ws.send(JSON.stringify({
+        type: "shell_start"
+    }));
 };
 
 ws.onmessage = (event) => {
-  console.log("Message:", event.data);
+    console.log("Message:", event.data);
 };
 
 ws.onclose = () => {
-  console.log("Disconnected");
+    console.log("Disconnected");
 };
 
 ws.onerror = (error) => {
-  console.error("WebSocket error:", error);
+    console.error("WebSocket error:", error);
 };
